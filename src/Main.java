dd73aae62355f83c837d33bab9252a8206be6025
SOVA <sova16tais@gmail.com>
2024-10-28 22:48:14 +0300

    Added output with two languages

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет Мир!");
+        System.out.println("Hello World!");
 
          var dog=8.0;
         System.out.println(dog);
